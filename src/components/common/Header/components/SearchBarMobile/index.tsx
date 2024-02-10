6ce7178099452421b9a444b1e019d62a4e11df66
OleksandrPanichuk <sashapanichuk@gmail.com>
2024-02-10 09:21:44 +0200

    Mobile search bar was added

---
+++ src/components/common/Header/components/SearchBarMobile/index.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { useClickOutside, useDebounce } from '@/hooks'
+import { SearchIcon, XIcon } from 'lucide-react'
+import { useTranslations } from 'next-intl'
+import { Fragment, useEffect, useRef, useState } from 'react'
+import { SearchResults } from '..'
+
+import { Transition } from '@headlessui/react'
+import styles from './SearchBarMobile.module.scss'
+
+export const SearchBarMobile = () => {
+	const [isVisible, setIsVisible] = useState<boolean>(false)
+	const [open, setOpen] = useState<boolean>(false)
+	const [searchValue, setSearchValue] = useState<string>('')
+
+	const debouncedSearchValue = useDebounce(searchValue)
+
+	const t = useTranslations('Header.SearchBar')
+
+	const popoverRef = useRef<HTMLDivElement>(null)
+	const labelRef = useRef<HTMLLabelElement>(null)
+
+	useClickOutside([popoverRef, labelRef], () => setOpen(false))
+
+	useEffect(() => {
+		setOpen(!!debouncedSearchValue)
+	}, [debouncedSearchValue])
+
+	const onFocus = () => {
+		if (debouncedSearchValue && !open) {
+			setOpen(true)
+		}
+	}
+
+	return (
+		<>
+			<button onClick={() => setIsVisible(true)}>
+				<SearchIcon />
+			</button>
+
+			<Transition
+				show={isVisible}
+				as={Fragment}
+				enter='ease-out duration-300'
+				enterFrom='opacity-0 top-[-100px]'
+				enterTo='opacity-100 top-[0px]'
+				leave='ease-in duration-200'
+				leaveFrom='opacity-100 top-[0px]'
+				leaveTo='opacity-0 top-[-100px]'
+			>
+				<div className={styles.container}>
+					<div className={styles.wrapper}>
+						<label className={styles.label} ref={labelRef}>
+							<SearchIcon />
+							<input
+								onFocus={onFocus}
+								aria-label='search products'
+								value={searchValue}
+								onChange={e => setSearchValue(e.target.value)}
+								placeholder={t('Placeholder')}
+							/>
+						</label>
+						<button onClick={() => setIsVisible(false)}>
+							<XIcon />
+						</button>
+					</div>
+
+					<SearchResults
+						ref={popoverRef}
+						isOpen={open}
+						resetSearchValue={() => {
+							setSearchValue('')
+							setIsVisible(false)
+						}}
+						searchValue={debouncedSearchValue}
+					/>
+				</div>
+			</Transition>
+		</>
+	)
+}
--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -7,11 +7,16 @@ import { Link } from '@/shared/navigation'
 import { Heart, ShoppingCart, User } from 'lucide-react'
 import styles from './Header.module.scss'
 import { LanguageSelect, MobileNavbar, Navbar, SearchBar } from './components'
+import { SearchBarMobile } from './components/SearchBarMobile'
 
 export const Header = () => {
 	return (
 		<header className={styles.header}>
-			<Visibility  breakpoint={`(min-width: ${cssVariables.screenLg})`}>
+			<Visibility
+				ssr
+				fallback={[true]}
+				breakpoint={`(min-width: ${cssVariables.screenLg})`}
+			>
 				<>
 					<div className={styles.logo}>
 						<Logo />
@@ -19,7 +24,11 @@ export const Header = () => {
 					<Navbar className={styles.navbar} />
 				</>
 			</Visibility>
-			<Visibility ssr fallback={[true]} breakpoint={`(max-width: ${cssVariables.screenLg})`}>
+			<Visibility
+				ssr
+				fallback={[true]}
+				breakpoint={`(max-width: ${cssVariables.screenLg})`}
+			>
 				<div className={styles['mobile-navbar']}>
 					<MobileNavbar />
 				</div>
@@ -27,9 +36,21 @@ export const Header = () => {
 			<div className={styles['language-select']}>
 				<LanguageSelect />
 			</div>
+
 			<div className={styles['right-side']}>
-				<Visibility ssr fallback={[true]} breakpoint={`(min-width: ${cssVariables.screenXs})`}>
+				<Visibility
+					ssr
+					fallback={[true]}
+					breakpoint={`(min-width: ${cssVariables.screenXs})`}
+				>
 					<SearchBar />
+				</Visibility>
+				<Visibility
+					ssr
+					fallback={[true]}
+					breakpoint={`(max-width: ${cssVariables.screenXs})`}
+				>
+					<SearchBarMobile />
 				</Visibility>
 				<ul className={styles['right-side__links']}>
 					<li>
